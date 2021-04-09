fc7cc8b5c99597e5d500c29817ca0888bebac1a2
RuffaloLavoisier <jsh970307@daum.net>
2021-04-09 19:08:55 +0900

    catch debug and add comment

--- nbackgame.c
+++ nbackgame.c
@@ -1,35 +1,25 @@
-
-#include<stdio.h>
-#include<stdlib.h>    // srand, rand
+#include<stdio.h> 	// standard input output
+#include<stdlib.h>  // srand, rand
 #include<time.h>    // time_t, clock, difftime
 
-//#define DELAY 3000    // 지연시간 5초
-#define N_BACK 3
+#define N_BACK 3 //what do you want game version ?
+
 int main(void)
 {
 	int n_back_mode = N_BACK;
 	int n_back_box[N_BACK + 1] = { 0 }; //n back 박스 생성
 
-	int score_r = 0;                // 정답 수 
-	time_t new_time, old_time;    // 경과 시간 
-	int ScoreStart=0;                    // 답 입력 여부 
+	int score_r = 0;                	// 정답 수 
+	time_t new_time, old_time;    		// 경과 시간 
+	int ScoreStart=0;                   // 답 입력 여부 
 	int count = 0;
-	int real_q_count = 0;
+	int real_q_count = 0;				// 첫 시작 카운트를 제외하고서 문제 수를 카운트
 
 	char prev_answer = 'Z';
 	char my_prev_answer = 'Z';
 
 	char answer = 'Z';
 	char my_answer = 'Z';
-
-
-	//-----------------------동적할당 세트
-	/*int* n_back_box;
-	int n_back_mode;
-	printf("How many N - BACKs would you like to challenge? : ");
-	scanf_s("%d", &n_back_mode);
-	n_back_box = (int*)malloc(sizeof(int) * n_back_mode);
-	free(n_back_box);*/
 
 	srand(time(NULL));        // 시작할 때마다 값이 달라지도록 함
 
@@ -60,48 +50,31 @@ int main(void)
 		}
 		n_back_box[n_back_mode] = (rand() % 9 + 1) * (rand() % 9 + 1) % 10;//한자리의 랜덤한 숫자 부여
 		count++;//랜덤 생성 횟수
+
 		if (n_back_box[0] == n_back_box[n_back_mode])//n-back 정답 추출
 			answer = 'o';
 		else
 			answer = 'x';
 
-//		old_time = clock();        // 시작 시간 
-//		keyin = 1;                // 키입력 초기값 
 		system("clear");
+		
 		if(count>=N_BACK+1){
 			printf("총 문제 : %d 맞은 갯수 : %d \n이전 정답 : %c 내가 쓴 정답 : %c \n", real_q_count, score_r, prev_answer, my_prev_answer);
 			ScoreStart = 1;
 		}
-		printf("Random : %d\n", n_back_box[n_back_mode]);
-/*		
-		do {
-			new_time = clock();    // 현재 시간 
-			if (difftime(new_time, old_time) > DELAY) {    // 시간 초과 검사 
-				my_answer = 'Z';        // 답이 없음 
-				keyin = 0;        // 키입력이 없음 
-				break;
-			}
-		} while (!kbhit());        // 키가 안 눌린 동안 
 
+		printf("Random : %d\n", n_back_box[n_back_mode]);  // 메인 랜덤 숫자
 
-		if (keyin == 1) {
-*/
-			scanf(" %c",&my_answer);       // 답을 받음 
-/*			new_time = clock();    // 현재 시간 
-			if (difftime(new_time, old_time) > DELAY) my_answer = 'Z';
+		scanf(" %c",&my_answer);       // 답을 받음
+
+		if (ScoreStart){
+			if (answer == my_answer) 
+				score_r++;    // 정답수 
 		}
-*/if (ScoreStart){
-		if (answer == my_answer) {
-			score_r++;    // 정답수 
-		}
-		else {
-			//printf("시간을 초과했거나 틀렸어요 정답은 %d입니다\n총 맞추신개수는 %d입니다\n", sum, score);
-			//printf("시간을 초과했거나 틀렸어요\n");
-			//return 0;
-		}}
 
 		my_prev_answer = my_answer;
 		prev_answer = answer;
+
 		if (count >= sizeof(n_back_box) / sizeof(n_back_box[0])) {
 			real_q_count++;
 		}
